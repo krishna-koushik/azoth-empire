ea09d02482b3d7c07ae14ec4f19b582f31aa21fe
Airaken <lucho92p@hotmail.com>
2021-09-21 21:59:17 -0500

    feat: Create wars query with pagination

--- azoth-empire-api/src/graphql/resolvers/war.resolver.js
+++ azoth-empire-api/src/graphql/resolvers/war.resolver.js
@@ -1,36 +1,41 @@
 const ConnectionModel = require('@application/application-model/connection.model');
 const ConnectionHandler = require('@application/application-handlers/connection.handler');
 
+const WarInfoRepository = require('../../database/repository/war-info.repository');
 const PlayerInfoRepository = require('../../database/repository/player-info.repository');
+
 const { UserInputError } = require('apollo-server');
 
 module.exports = {
-    PlayerOrderField: {
-        NAME: 'name',
-        GUILD_NAME: 'guild',
-        JOIN_DATE: 'joinDate',
-        ACTIVE: 'active',
+    WarOrderField: {
+        ATTACKER: 'attacker.guild',
+        DEFENDER: 'defender.guild',
+        WAR_TIME: 'time',
+        WAR_TYPE: 'warType',
     },
-    PlayerEdge: {
-        node: obj => PlayerInfoRepository.getById(obj.node),
+    WarEdge: {
+        node: obj => WarInfoRepository.getById(obj.node),
+    },
+    ArmyPlayer: {
+        player: obj => PlayerInfoRepository.getById(obj.playerId),
     },
     Query: {
-        async player(obj, args, context) {
+        async war(obj, args, context) {
             const { event: { requestContext } = {} } = context;
             if (!requestContext) {
                 return null;
             }
 
-            return PlayerInfoRepository.getById(args.id);
+            return WarInfoRepository.getById(args.id);
         },
-        async players(obj, args, context) {
+        async wars(obj, args, context) {
             const { event: { requestContext } = {} } = context;
             if (!requestContext) {
                 return null;
             }
             try {
                 const m = new ConnectionModel(args);
-                return new ConnectionHandler('players').handle(m);
+                return new ConnectionHandler('wars').handle(m);
             } catch (e) {
                 console.error(e);
                 throw new UserInputError(e.message);

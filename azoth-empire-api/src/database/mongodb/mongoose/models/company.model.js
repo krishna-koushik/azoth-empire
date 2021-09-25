7d17bd4e0b6ebfec78a98a0f149a111c7fab661f
krishnakoushik <krishna.koushik.n@gmail.com>
2021-09-24 20:40:51 -0400

    fix: changing war collection changes

--- azoth-empire-api/src/database/mongodb/mongoose/models/company.model.js
+++ azoth-empire-api/src/database/mongodb/mongoose/models/company.model.js
@@ -1,10 +1,31 @@
 const Mongoose = require('mongoose');
-const Schema = Mongoose.Schema;
+const {
+    Schema,
+    Schema: {
+        Types: { ObjectId },
+    },
+} = Mongoose;
 
 let companySchema = new Schema({
     name: {
         type: String,
         index: true,
+    },
+    factionId: {
+        type: ObjectId,
+        ref: 'factions',
+        index: true,
+    },
+    faction: String,
+    guildId: {
+        type: ObjectId,
+        ref: 'guilds',
+        index: true,
+    },
+    guild: String,
+    active: {
+        type: Boolean,
+        default: true,
     },
 });
 

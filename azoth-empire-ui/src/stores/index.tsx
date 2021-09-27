1d4d5b481219c0b7d40ab08c4c09f9bcbd449e7c
krishnakoushik <krishna.koushik.n@gmail.com>
2021-09-26 23:28:15 -0400

    fix: facepalm. plural

--- azoth-empire-ui/src/stores/index.tsx
+++ azoth-empire-ui/src/stores/index.tsx
@@ -5,13 +5,13 @@ const { state, onChange } = createStore({
     currentPlayerId: authService.getId() || '',
     currentRoles: authService.getRoles() || [],
     token: authService.getToken() || '',
-    isLeader: (authService.getRoles() || []).includes('Leader'),
+    isLeader: (authService.getRoles() || []).includes('Leaders'),
     isRegular: (authService.getRoles() || []).includes('NewWorld'),
     isInActive: (authService.getRoles() || []).includes('inactive'),
 });
 
 onChange('currentRoles', value => {
-    state.isLeader = value.includes('Leader');
+    state.isLeader = value.includes('Leaders');
     state.isRegular = value.includes('NewWorld');
     state.isInActive = value.includes('inactive');
 });
